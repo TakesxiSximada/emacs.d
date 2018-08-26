2fc806a37b0b63736813293dd6c464e9eaa7c3b0
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2018-08-27 05:43:46 +0900

    Add startup aliases.

---
+++ env.sh
@@ -0,0 +1,5 @@
+export OUR_EMACS="/srv/ge"
+alias ew='emacs -q --no-site-file --no-splash --no-x-resources -f package-initialize --debug-init -nw --load .emacs.d/init.el'
+alias ewq='emacs -q --no-site-file --no-splash --no-x-resources -f package-initialize --debug-init -nw'
+alias ge='emacs -q --no-site-file --no-splash --no-x-resources -f package-initialize --debug-init --load .emacs.d/init.el'
+alias geq='emacs -q --no-site-file --no-splash --no-x-resources -f package-initialize --debug-init'
