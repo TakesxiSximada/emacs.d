fb6541f057c61f476d80331f9b319a049a9d89a7
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2018-08-28 00:34:45 +0900

    Add restclient and http mode

---
+++ http/.emacs.d/init.el
@@ -0,0 +1,19 @@
+;; -*- lexical-binding: t -*-
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-common-dir (or (getenv "OUR_EMACS") nil))
+(setq user-before-file (if user-common-dir (format "%s/before.el" user-common-dir) nil))
+(setq user-additional-file (if user-common-dir (format "%s/additional.el" user-common-dir) nil))
+(setq user-after-file (if user-common-dir (format "%s/after.el" user-common-dir) nil))
+(when (file-exists-p user-before-file) (load-file user-before-file))
+(when (file-exists-p user-additional-file) (load-file user-additional-file))
+(when (file-exists-p user-after-file)
+  (add-hook 'emacs-startup-hook (lambda () (load-file user-after-file))))
+
+
+(require 'use-package)
+
+(use-package http :ensure t :defer t)
+(use-package restclient :ensure t
+	     :mode (("\\.http\\'" . restclient-mode))
+	     :bind (:map restclient-mode-map
+			 ("C-c C-e" . http-edit-body-indirect)))
