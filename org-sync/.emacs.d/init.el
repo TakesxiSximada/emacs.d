163eb0c647b40b27f277fa72016c9c1963ff2e9d
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2018-08-28 12:32:06 +0900

    Add org-sync settings

---
+++ org-sync/.emacs.d/init.el
@@ -0,0 +1,21 @@
+;; -*- lexical-binding: t -*-
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-common-dir (or (getenv "OUR_EMACS") nil))
+(setq user-before-file (if user-common-dir (format "%s/before.el" user-common-dir) nil))
+(setq user-additional-file (if user-common-dir (format "%s/additional.el" user-common-dir) nil))
+(setq user-after-file (if user-common-dir (format "%s/after.el" user-common-dir) nil))
+(when (file-exists-p user-before-file) (load-file user-before-file))
+(when (file-exists-p user-additional-file) (load-file user-additional-file))
+(when (file-exists-p user-after-file)
+  (add-hook 'emacs-startup-hook (lambda () (load-file user-after-file))))
+
+
+(require 'use-package)
+
+(use-package org-sync :ensure t 
+  :config
+  (require 'org-sync-github))
+
+(load-file "~/.config/github/org-sync.el")
+
+
