469ecd7f2ba1b48a1cddad244609b672cd84f961
sximada <50688746+sximada@users.noreply.github.com>
2019-08-14 12:52:40 +0900

    Add initial package settings

---
+++ init.el
@@ -0,0 +1,10 @@
+;; -*- coding: utf-8 -*-
+(setq custom-file (locate-user-emacs-file "custom.el"))
+
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")
+	("org" . "https://orgmode.org/elpa/")
+	;; ("marmalade" . "http://marmalade-repo.org/packages/")
+	;; ("melpa-stable" . "http://stable.melpa.org/packages/")
+	)
