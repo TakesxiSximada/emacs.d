344fd01be75a04dd2024a8fe738680c2ec32f6d4
sximada <50688746+sximada@users.noreply.github.com>
2019-08-14 14:07:46 +0900

    Add Our Async Exec

--- init.el
+++ init.el
@@ -30,6 +30,176 @@
   (setq ido-vertical-show-count t)
   )
 
+;;; Our Async Exec
+(defvar our-async-exec-cmd-history nil)
+(defvar our-async-exec-cwd-history nil)
+(defvar our-async-exec-cmd nil)
+
+(defun our-create-buffer-name (cmd &optional cwd)
+  (format "`%s`%s"
+	  (let ((elms (split-string cmd)))
+	    (mapconcat 'identity
+		       (append (last (split-string (car elms) "/"))
+			       (cdr elms))
+		       " "))
+	  (if (and cwd (> (length cwd) 0)) (format ": %s" cwd) "")))
+
+
+(defun our-async-exec (cmd &optional cwd buffer)
+  (let ((buf (or buffer (get-buffer-create (our-create-buffer-name cmd cwd)))))
+    (with-current-buffer buf
+      (setq-local default-directory (or cwd default-directory))
+      (async-shell-command cmd buf)
+      ;; (our-async-exec-mode)
+      (setq-local default-directory (or cwd default-directory))
+      (setq-local our-async-exec-cmd cmd))))
+
+
+(defun our-async-exec-retry ()
+  (interactive)
+  (if-let* ((cmd our-async-exec-cmd))
+      (progn
+	(async-shell-command our-async-exec-cmd (current-buffer))
+	(our-async-exec-mode)
+	(setq-local our-async-exec-cmd cmd))))
+
+
+(defun our-async-exec-interactive (cmd &optional cwd buffer)
+  (interactive
+   (list (read-string "Command: "
+		      ""
+		      'our-async-exec-cmd-history
+		      "")
+	 (read-string "Directory: "
+		      default-directory
+		      'our-async-exec-cwd-history
+		      default-directory)))
+  (our-async-exec cmd cwd buffer))
+
+(defun our-async-exec-close ()
+  (interactive)
+  (kill-buffer (current-buffer)))
+
+
+(defun our-get-buffer-create (&optional name)
+  (interactive "sBuffer Name: ")
+  (let ((buf-name (format "*%s*" name)))
+    (get-buffer-create buf-name)
+    (message (format "Created a buffer: %s" buf-name))))
+
+
+;; MONKEY PATCHING
+(defun comint-output-filter (process string)
+  (let ((oprocbuf (process-buffer process)))
+    ;; First check for killed buffer or no input.
+    (when (and string oprocbuf (buffer-name oprocbuf))
+      (with-current-buffer oprocbuf
+	;; Run preoutput filters
+	(let ((functions comint-preoutput-filter-functions))
+	  (while (and functions string)
+	    (if (eq (car functions) t)
+		(let ((functions
+                       (default-value 'comint-preoutput-filter-functions)))
+		  (while (and functions string)
+		    (setq string (funcall (car functions) string))
+		    (setq functions (cdr functions))))
+	      (setq string (funcall (car functions) string)))
+	    (setq functions (cdr functions))))
+
+	;; Insert STRING
+	(let ((inhibit-read-only t)
+              ;; The point should float after any insertion we do.
+	      (saved-point (copy-marker (point) t)))
+
+	  ;; We temporarily remove any buffer narrowing, in case the
+	  ;; process mark is outside of the restriction
+	  (save-restriction
+	    (widen)
+
+	    (goto-char (process-mark process))
+	    (set-marker comint-last-output-start (point))
+
+            ;; Try to skip repeated prompts, which can occur as a result of
+            ;; commands sent without inserting them in the buffer.
+            (let ((bol (save-excursion (forward-line 0) (point)))) ;No fields.
+              (when (and (not (bolp))
+                         (looking-back comint-prompt-regexp bol))
+                (let* ((prompt (buffer-substring bol (point)))
+                       (prompt-re (concat "\\`" (regexp-quote prompt))))
+                  (while (string-match prompt-re string)
+                    (setq string (substring string (match-end 0)))))))
+            (while (string-match (concat "\\(^" comint-prompt-regexp
+                                         "\\)\\1+")
+                                 string)
+              (setq string (replace-match "\\1" nil nil string)))
+
+	    ;; insert-before-markers is a bad thing. XXX
+	    ;; Luckily we don't have to use it any more, we use
+	    ;; window-point-insertion-type instead.
+	    (insert string)
+
+	    ;; Advance process-mark
+	    (set-marker (process-mark process) (point))
+
+	    (unless comint-inhibit-carriage-motion
+	      ;; Interpret any carriage motion characters (newline, backspace)
+	      (comint-carriage-motion comint-last-output-start (point)))
+
+	    ;; Run these hooks with point where the user had it.
+	    (goto-char saved-point)
+	    (run-hook-with-args 'comint-output-filter-functions string)
+	    (set-marker saved-point (point))
+
+	    (goto-char (process-mark process)) ; In case a filter moved it.
+
+	    (unless comint-use-prompt-regexp
+              (with-silent-modifications
+                (add-text-properties comint-last-output-start (point)
+                                     '(front-sticky
+				       (field inhibit-line-move-field-capture)
+				       rear-nonsticky t
+				       field output
+				       inhibit-line-move-field-capture t))))
+
+	    ;; Highlight the prompt, where we define `prompt' to mean
+	    ;; the most recent output that doesn't end with a newline.
+	    (let ((prompt-start (save-excursion (forward-line 0) (point)))
+		  (inhibit-read-only t))
+	      (when comint-prompt-read-only
+		(with-silent-modifications
+		  (or (= (point-min) prompt-start)
+		      (get-text-property (1- prompt-start) 'read-only)
+		      (put-text-property (1- prompt-start)
+					 prompt-start 'read-only 'fence))
+		  (add-text-properties prompt-start (point)
+				       '(read-only t front-sticky (read-only)))))
+	      (when comint-last-prompt
+		;; There might be some keywords here waiting for
+		;; fontification, so no `with-silent-modifications'.
+		(font-lock--remove-face-from-text-property
+		 (car comint-last-prompt)
+		 (cdr comint-last-prompt)
+		 'font-lock-face
+		 'comint-highlight-prompt))
+	      (setq comint-last-prompt
+		    (cons (copy-marker prompt-start) (point-marker)))
+	      ;; ここのプロパティ設定によりとてつもなく処理がおもくなるためコメントアウト
+	      ;; (font-lock-prepend-text-property prompt-start (point)
+	      ;; 				       'font-lock-face
+	      ;; 				       'comint-highlight-prompt)
+	      (add-text-properties prompt-start (point) '(rear-nonsticky t)))
+	    (goto-char saved-point)))))))
+
+
+(easy-mmode-define-minor-mode our-async-exec-mode
+			      "This is our-async-exec-mode"
+			      nil
+			      "OurAsyncExec"
+			      '(("C-c C-g" . our-async-exec-retry)
+				("C-c C-q" . our-async-exec-close)))
+
+;;; Our Async Exec Ends here.
+
 (defun rust-lang-install ()
   (interactive)
   (async-shell-command "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh)"))
@@ -52,4 +222,7 @@
 
  ;; Git
  ("C-x C-v" . magit-status)
+
+ ;; My Customize
+ ("M-_" . our-async-exec-interactive)
  )
