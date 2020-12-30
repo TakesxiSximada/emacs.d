a7d24e65703f4f01ecce2cf846eeb14938f230f1
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2020-12-30 10:02:49 +0900

    Add preojectile configuration.

--- init.el
+++ init.el
@@ -1388,3 +1388,8 @@ The build string will be of the format:
 (bind-keys*
  ("C-t C-p" . projectile-switch-project)
  )
+
+(add-hook 'projectile-after-switch-project-hook  #'configur-after-project-for-projectile)
+(projectile-mode)
+(custom-set-variables
+ '(projectile-switch-project-action #'projectile-dired))
