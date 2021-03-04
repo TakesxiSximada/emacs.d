63b18d2feb305aecc988f929c283bdf3758345ca
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2021-03-04 09:16:15 +0900

    Support escape sequence in compilation mode.

--- init.el
+++ init.el
@@ -1653,3 +1653,64 @@ The build string will be of the format:
             (setq js-indent-level 2)))
 
 (message "Finished to initialize emacs")
+
+
+
+;;; for compilation-mode ansi escaping
+(add-hook 'term-mode-hook 'compilation-shell-minor-mode)
+
+(require 'ansi-color)
+
+
+(defun endless/colorize-compilation ()
+  "Colorize from `compilation-filter-start' to `point'."
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region
+     compilation-filter-start (point))))
+
+(add-hook 'compilation-filter-hook
+          #'endless/colorize-compilation)
+
+
+;; Stolen from (https://oleksandrmanzyuk.wordpress.com/2011/11/05/better-emacs-shell-part-i/)
+(progn 
+  (defun regexp-alternatives (regexps)
+    "Return the alternation of a list of regexps."
+    (mapconcat (lambda (regexp)
+		 (concat "\\(?:" regexp "\\)"))
+               regexps "\\|"))
+
+  (defvar non-sgr-control-sequence-regexp nil
+    "Regexp that matches non-SGR control sequences.")
+
+  (setq non-sgr-control-sequence-regexp
+	(regexp-alternatives
+	 '(;; icon name escape sequences
+           "\033\\][0-2];.*?\007"
+           ;; non-SGR CSI escape sequences
+           "\033\\[\\??[0-9;]*[^0-9;m]"
+           ;; noop
+           "\012\033\\[2K\033\\[1F"
+           )))
+
+  (defun filter-non-sgr-control-sequences-in-region (begin end)
+    (save-excursion
+      (goto-char begin)
+      (while (re-search-forward
+              non-sgr-control-sequence-regexp end t)
+	(replace-match ""))))
+
+  (defun filter-non-sgr-control-sequences-in-output (ignored)
+    (let ((start-marker
+           (or comint-last-output-start
+               (point-min-marker)))
+          (end-marker
+           (process-mark
+            (get-buffer-process (current-buffer)))))
+      (filter-non-sgr-control-sequences-in-region
+       start-marker
+       end-marker)))
+
+  (add-hook 'comint-output-filter-functions
+            'filter-non-sgr-control-sequences-in-output)
+  )
