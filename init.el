0d466030a26f514688feae087f39ed66c1f9f114
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2021-07-08 08:39:55 +0900

    Update docker build coomand.

--- init.el
+++ init.el
@@ -577,7 +577,7 @@ The build string will be of the format:
   (save-buffer)
     (compilation-start
         (format
-            "%s%s build --progress plain %s %s %s -f %s %s"  ;; FIX
+            "%s%s build --progress plain --ssh=default %s %s %s -f %s %s"  ;; FIX
             (if dockerfile-use-sudo "sudo " "")
             dockerfile-mode-command
             (if no-cache "--no-cache" "")
