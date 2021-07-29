80c50bc01dc4e39e4b99a7b80c572b0f21277951
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2021-07-29 10:03:42 +0900

    Add our lisp.

--- init.el
+++ init.el
@@ -672,6 +672,19 @@ The buffer contains the raw HTTP response sent by the server."
 (setenv "CPPFLAGS" (string-join '("-I/usr/local/opt/openjdk/include")))
 
 ;; -----------------------------
+;; our
+;; -----------------------------
+(defun our-buffer-copy-current-file-path ()
+  "バッファのファイルパスをクリップボードにコピーする"
+  (interactive)
+  (let ((path (buffer-file-name)))
+    (if path
+  	(progn
+          (kill-new path)
+          (message (format "Copied: %s" path)))
+      (message (format "Cannot copied")))))
+
+;; -----------------------------
 ;; CUSTOM
 ;; -----------------------------
 (load-file "/opt/ng/symdon/emacs.d/after.el")
