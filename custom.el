1bdb91979cd7e9c1eea9016c59d6f6f7d779756e
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2022-02-15 19:07:38 +0900

    Add s3ed.

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(modus-themes solarized-theme org-agenda-property flycheck-pos-tip editorconfig wgrep-ag web vue-mode vterm vagrant-tramp unicode-escape typescript-mode twilight-bright-theme terraform-mode smex slime request quelpa-use-package pyvenv py-isort pcre2el ox-gfm org-re-reveal ob-restclient ob-async nginx-mode monky mew markdown-mode magit js2-mode ido-vertical-mode ido-completing-read+ haskell-mode grip-mode google-translate go-mode github-review flycheck fakir el-get eglot dockerfile-mode docker-compose-mode docker dired-filter db company blacken avy-menu ag adoc-mode add-node-modules-path)))
+   '(s3ed modus-themes solarized-theme org-agenda-property flycheck-pos-tip editorconfig wgrep-ag web vue-mode vterm vagrant-tramp unicode-escape typescript-mode twilight-bright-theme terraform-mode smex slime request quelpa-use-package pyvenv py-isort pcre2el ox-gfm org-re-reveal ob-restclient ob-async nginx-mode monky mew markdown-mode magit js2-mode ido-vertical-mode ido-completing-read+ haskell-mode grip-mode google-translate go-mode github-review flycheck fakir el-get eglot dockerfile-mode docker-compose-mode docker dired-filter db company blacken avy-menu ag adoc-mode add-node-modules-path)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
