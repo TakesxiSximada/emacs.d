6a24fe7ea4a13bad324c1e318a5a63a5fa197e04
TakesxiSximada <sximada@gmail.com>
2022-05-30 21:40:07 +0900

    Update custom.el.

--- custom.el
+++ custom.el
@@ -1,68 +1,10 @@
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(
-     add-node-modules-path
-     adoc-mode
-     ag
-     avy-menu
-     blacken
-     company
-     csv-mode
-     db
-     ddskk
-     dired-filter
-     docker
-     docker-compose-mode
-     dockerfile-mode
-     editorconfig
-     eglot
-     el-get
-     fakir
-     flycheck
-     flycheck-pos-tip
-     github-review
-     go-mode
-     google-translate
-     grip-mode
-     haskell-mode
-     http
-     ido-completing-read+
-     ido-vertical-mode
-     js2-mode
-     markdown-mode
-     mew
-     monky
-     nginx-mode
-     ob-async
-     ob-restclient
-     org-agenda-property
-     org-re-reveal
-     org-super-agenda
-     ox-gfm
-     pcre2el
-     py-isort
-     pyvenv
-     quelpa-use-package
-     request
-     s3ed
-     slime
-     smex
-     solarized-theme
-     terraform-mode
-     twilight-bright-theme
-     typescript-mode
-     unicode-escape
-     vagrant-tramp
-     vterm
-     vue-mode
-     web
-     wgrep-ag
-     ))
+   '(testing ob-http add-node-modules-path adoc-mode ag avy-menu blacken company csv-mode db ddskk dired-filter docker docker-compose-mode dockerfile-mode editorconfig el-get fakir flycheck flycheck-pos-tip github-review go-mode google-translate grip-mode haskell-mode http ido-completing-read+ ido-vertical-mode js2-mode markdown-mode mew monky nginx-mode ob-async ob-restclient org-agenda-property org-re-reveal org-super-agenda ox-gfm pcre2el py-isort pyvenv quelpa-use-package request s3ed slime smex solarized-theme terraform-mode twilight-bright-theme typescript-mode unicode-escape vagrant-tramp vterm vue-mode web wgrep-ag))
  '(warning-suppress-log-types '((use-package) (use-package)))
  '(warning-suppress-types '((use-package))))
 (custom-set-faces
