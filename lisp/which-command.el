2d2e7523c3106ce51d33f345a123a1e311cb1d1e
TakesxiSximada <sximada@gmail.com>
2022-06-03 09:11:42 +0900

    Fix which command.

--- lisp/which-command.el
+++ lisp/which-command.el
@@ -1,14 +1,34 @@
-;;; which-command.el --- Run which command on Emacs  -*- lexical-binding: t -*-
+;;; which-command --- Run which command on Emacs  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2022 TakesxiSximada
 
-;; Author: TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
-;; Maintainer: TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
+
+;; Author: TakesxiSximada <sximada@gmail.com>
+;; Maintainer: TakesxiSximada <sximada@gmail.com>
 ;; Repository:
-;; Version: 1
+;; Version: 2
 ;; Package-Version: 20220426.0000
-;; Package-Requires: ((emacs "25.1") (s "1.12.0"))
-;; Date: 2022-04-26
+;; Package-Requires: ((emacs "27.1") (s "1.12.0"))
+;; Date: 2022-05-29
+
+;; This file is part of which-command.
+
+;; which-command is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU Affero General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; which-command is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public
+;; License along with this program.  If not, see
+;; <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
 
 ;; This package is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Affero General Public License
