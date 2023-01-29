6cdb291257ad414874e05a697a797dd40e9b47bc
TakesxiSximada <sximada@gmail.com>
2023-01-29 18:28:58 +0900

    fix: ignore byte compile warnings

--- early-init.el
+++ early-init.el
@@ -2,3 +2,4 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(setq byte-compile-warnings nil)
