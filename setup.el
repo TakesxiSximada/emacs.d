e44a8314154e3de9cec68de1a35ecb4c3637cb20
TakesxiSximada <sximada@gmail.com>
2025-04-04 15:37:14 +0900

    remove unnecessary files

--- setup.el
+++
@@ -1,9 +0,0 @@
-;; -*- coding: utf-8 -*-
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
-(package-refresh-contents)
-(package-install 'use-package)
-
