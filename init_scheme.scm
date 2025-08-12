0a5fd03ea24f29899f4a7aff16cc40a337bf5d5b
TakesxiSximada <sximada@gmail.com>
2025-08-13 08:17:40 +0900

    add scheme initialize file

---
+++ init_scheme.scm
@@ -0,0 +1,9 @@
+;; -*- coding: UTF-8 -*-
+
+(display "
+###############################################
+#         HELLO LISP WORLD!!                  #
+#                                             #
+# This session is working on TinyScheme 1.42. #
+###############################################
+")
