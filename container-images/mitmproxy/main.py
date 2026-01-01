e588284e1e4dc9acaed93ed178b197603dd1fc22
TakesxiSximada <sximada@gmail.com>
2026-01-02 03:25:35 +0900

    remove container-image files

--- container-images/mitmproxy/main.py
+++
@@ -1,8 +0,0 @@
-from mitmproxy import http
-
-
-def request(flow: http.HTTPFlow) -> None:
-    if flow.request.path == "/aws":
-        flow.response = http.Response.make(200, content=b"{}")
-        return
-    flow.response = http.Response.make(200, content=b"Hello, world!")
